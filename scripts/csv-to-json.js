5d9d8505de43792a5d8e54d9da01df9b1bcdda56
sethvincent <sethvincent@gmail.com>
2021-08-27 08:58:44 -0700

    remove unused dependency

--- scripts/csv-to-json.js
+++ scripts/csv-to-json.js
@@ -5,7 +5,6 @@ import { readCsv } from '@njam-data/tools/csv.js'
 import { writeJson } from '@njam-data/tools/json.js'
 
 const dataDirectory = dirname.join(import.meta.url, '..', 'data')
-const sourceDirectory = path.join(dataDirectory, 'source')
 const processedDirectory = path.join(dataDirectory, 'processed')
 const csvFilepath = path.join(processedDirectory, 'big-local-news', '03_county_sub_pl94171_standard_compare_2010_2020.csv')
 const jsonFilepath = path.join(processedDirectory, 'big-local-news', '03_county_sub_pl94171_standard_compare_2010_2020.json')
