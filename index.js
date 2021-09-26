972966bb4019f59f070ddc37bb33dc4c2a8420a6
sethvincent <sethvincent@gmail.com>
2021-09-26 15:08:31 -0700

    add getPopulation function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { readJson } from '@njam-data/tools/json.js'
 import { readCsv } from '@njam-data/tools/csv.js'
 
-import { filepaths } from './lib/filepaths.js'
+import { filepaths, totalPopulationJsonFilepath } from './lib/filepaths.js'
 
 const readers = {
   json: readJson,
@@ -11,7 +11,7 @@ const readers = {
 /**
  * get 2020 census data
  * @param object options
- * @param string options.dataset - default: 'county_subdivision'
+ * @param string options.dataset - default: 'county_subdivision'. options: state, county, county_subdivision, place, tract
  * @param string options.format - default: 'json'
  */
 export async function getData (options = {}) {
@@ -28,4 +28,23 @@ export async function getData (options = {}) {
   }
 
   return reader(filepath)
+}
+
+/**
+ * get 2020 population data
+ * @param object [options]
+ * @param string [options.dataset] - default: null, which returns all types. options: state, county, county_subdivision, place, tract
+ */
+export async function getPopulation (options = {}) {
+  const { dataset } = options
+
+  const data = await readJson(totalPopulationJsonFilepath)
+
+  if (!dataset) {
+    return data
+  }
+
+  return data.filter((row) => {
+    return row.admin_type === dataset
+  })
 }
--- scripts/csv-to-json.js
+++ scripts/csv-to-json.js
@@ -1,11 +1,19 @@
-import { readCsv } from '@njam-data/tools/csv.js'
+import { readCsv, writeCsv } from '@njam-data/tools/csv.js'
 import { writeJson } from '@njam-data/tools/json.js'
 import { convertNumberProperties } from '@njam-data/tools/convert.js'
 
-import { filepaths } from '../lib/filepaths.js'
+import {
+  filepaths,
+  totalPopulationCsvFilepath,
+  totalPopulationJsonFilepath
+} from '../lib/filepaths.js'
 
 async function main () {
-  Object.keys(filepaths).every(async (key) => {
+  const totalPopulation = []
+
+  const keys = Object.keys(filepaths)
+  
+  for (const key of keys) {
     const { csv, json } = filepaths[key]
     let rows = await readCsv(csv, { headers: true })
 
@@ -15,17 +23,54 @@ async function main () {
         if (row.state_abbr === 'NJ') {
           return true
         }
+
         return false
       }
 
       return true
     })
     .map((row) => {
-      return convertNumberProperties(row)
+      const props = convertNumberProperties(row)
+
+      let code
+      let name
+
+      if (key === 'state') {
+        code = props.state_code
+        name = props.state_name
+      } else if (key === 'county') {
+        code = props.county_code
+        name = row.county_name
+      } else if (key === 'county_subdivision'){
+        code = props.cousub_code
+        name = props.cousub_name
+      } else if (key === 'place'){
+        code = props.place_code
+        name = props.place_name
+      } else if (key === 'tract'){
+        code = props.tract_code
+        name = props.tract_name
+      }
+
+      if (props.cousub_name && props.cousub_name === 'County subdivisions not defined') {
+        // skip undefined county subdivisions
+      } else {
+        totalPopulation.push({
+          name,
+          code,
+          admin_type: key,
+          population: props['2020_pop']
+        })
+      }
+
+      return props
     })
 
     await writeJson(json, rows)
-  })
+  }
+
+  await writeJson(totalPopulationJsonFilepath, totalPopulation)
+  await writeCsv(totalPopulationCsvFilepath, totalPopulation)
 }
 
 main()
